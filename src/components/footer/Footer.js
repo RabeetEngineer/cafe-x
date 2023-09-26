940b3aa133bcc92b3730efcc6791e6b9ddc68497
Hafiz Rabeet Ahmad <muhammadrabeet@gmail.com>
2023-09-27 01:00:24 +0500

    contact us page started

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,7 +5,6 @@ const Footer = () => {
   return (
     <>
       <footer className="container-fluid mt-5 ">
-
         <div className="container">
           <div className="row mb-5">
             <div className="col-lg-2 col-md-2 col-sm-6 text-center">
@@ -33,11 +32,11 @@ const Footer = () => {
                 Sunday: 13:30 – 00:00
               </h5>
             </div>
-            <div className="col-lg-2 col-md-2 col-sm-6 text-center mb-5">
+            <div className="col-lg-2 col-md-2 col-sm-2 text-center mb-5">
               <h4 className="mt-3" style={{ fontWeight: "bolder" }}>
                 Social Media
               </h4>
-              <div className="social-media-icons mt-4 d-flex justify-content-between">
+              <div className="social-media-icons mt-4 d-flex justify-content-center gap-4">
                 <a className="anchor-null" href="">
                   <i class="fa fa-instagram" style={{ fontSize: "24px" }}></i>
                 </a>
@@ -92,9 +91,15 @@ const Footer = () => {
           </div>
         </div>
 
-        <div className="text-center">
-        <p style={{fontSize:"14px"}}>&copy; {new Date().getFullYear()} Cafe-X | All Rights Reserved</p>
-      </div>
+        <div className="container text-center">
+          <div className="row">
+            <div className="col-12">
+              <p style={{ fontSize: "14px" }}>
+                &copy; {new Date().getFullYear()} Cafe-X | All Rights Reserved
+              </p>
+            </div>
+          </div>
+        </div>
       </footer>
     </>
   );
--- src/contactUs/ContactUs.js
+++ src/contactUs/ContactUs.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import "../components/Navbar.css";
 
 const ContactUs = () => {
   return (
-    <div>ContactUs</div>
-  )
-}
+    <>
+      <div className="container-fluid">
+        <div className="container">
+          <h1 className="">Contact Us</h1>
+          <div className="row">
+            <div className="col-lg-8 col-md-8 col-sm-12 d-flex contact-card-main ">
+              <div className="contact-card d-flex flex-column gap-2 mt-5 align-items-center">
+                <i class="fa fa-facebook" style={{ fontSize: "36px", color:"#E0DDAA" }}></i>
+                <p  style={{ fontSize: "20px", color:"#E9E8E8" }}>
+                  Admin Officer
+                </p>
+                <p style={{ fontSize: "20px" }}>0324-1234567</p>
+              </div>
+              <div className="contact-card d-flex flex-column gap-2 mt-5 align-items-center">
+                <i class="fa fa-facebook" style={{ fontSize: "24px" }}></i>
+                <p  style={{ fontSize: "20px", color:"#E9E8E8" }}>
+                General Manager
+                </p>
+                <p style={{ fontSize: "20px" }}>0336-xxxxxx</p>
+              </div>
+              <div className="contact-card d-flex flex-column gap-2 mt-5 align-items-center">
+              <i class="fa fa-envelope" style={{ fontSize: "24px" }}></i>
+                <p  style={{ fontSize: "20px", color:"#E9E8E8" }}>
+                Inquiries
+                </p>
+                <p style={{ fontSize: "20px" }}>cafe-x@gmail.com</p>
+                <i class="fas fa-mobile-alt"></i> 
+              </div>
+            </div>
 
-export default ContactUs
+            <div className="col-lg-4 col-md-4 col-sm-12 contact-form-main">
+              <h5 style={{color:"black"}}>Instant Inquiry/Feedback*</h5>
+            </div>
+          </div>
+          <div className="row"></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ContactUs;
