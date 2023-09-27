d7655c4324593a864e3c524c86e8dc7a704bbfea
Hafiz Rabeet Ahmad <muhammadrabeet@gmail.com>
2023-09-27 23:48:05 +0500

    contact page created

--- src/contactUs/ContactUs.js
+++ src/contactUs/ContactUs.js
@@ -5,39 +5,67 @@ const ContactUs = () => {
   return (
     <>
       <div className="container-fluid">
-        <div className="container">
-          <h1 className="">Contact Us</h1>
-          <div className="row">
-            <div className="col-lg-8 col-md-8 col-sm-12 d-flex contact-card-main ">
-              <div className="contact-card d-flex flex-column gap-2 mt-5 align-items-center">
-                <i class="fa fa-facebook" style={{ fontSize: "36px", color:"#E0DDAA" }}></i>
-                <p  style={{ fontSize: "20px", color:"#E9E8E8" }}>
-                  Admin Officer
-                </p>
-                <p style={{ fontSize: "20px" }}>0324-1234567</p>
-              </div>
-              <div className="contact-card d-flex flex-column gap-2 mt-5 align-items-center">
-                <i class="fa fa-facebook" style={{ fontSize: "24px" }}></i>
-                <p  style={{ fontSize: "20px", color:"#E9E8E8" }}>
-                General Manager
-                </p>
-                <p style={{ fontSize: "20px" }}>0336-xxxxxx</p>
-              </div>
-              <div className="contact-card d-flex flex-column gap-2 mt-5 align-items-center">
-              <i class="fa fa-envelope" style={{ fontSize: "24px" }}></i>
-                <p  style={{ fontSize: "20px", color:"#E9E8E8" }}>
-                Inquiries
-                </p>
-                <p style={{ fontSize: "20px" }}>cafe-x@gmail.com</p>
-                <i class="fas fa-mobile-alt"></i> 
-              </div>
-            </div>
-
-            <div className="col-lg-4 col-md-4 col-sm-12 contact-form-main">
-              <h5 style={{color:"black"}}>Instant Inquiry/Feedback*</h5>
+        <div className="container ">
+          <div className="row justify-content-center align-items-center mt-5">
+            <div className="col-lg-6">
+              <form>
+                <h4 className="mb-5 text-center">Contact Us</h4>
+                <div className="row g-5">
+                  <div className="col-md-12 col-sm-12">
+                    <input
+                      placeholder="Name"
+                      type="text"
+                      className="form-control custom-input"
+                      id="your-name"
+                      name="your-name"
+                      required
+                    />
+                  </div>
+                  <div className="col-md-12">
+                    <input
+                      placeholder="Email"
+                      type="email"
+                      className="form-control custom-input"
+                      id="your-email"
+                      name="your-email"
+                      required
+                    />
+                  </div>
+                  <div className="col-md-12">
+                    <input
+                      placeholder="Subject"
+                      type="text"
+                      className="form-control custom-input"
+                      id="your-subject"
+                      name="your-subject"
+                    />
+                  </div>
+                  <div className="col-12">
+                    <textarea
+                      placeholder="Message"
+                      className="form-control custom-textarea"
+                      id="your-message"
+                      name="your-message"
+                      rows="5"
+                      required
+                    ></textarea>
+                  </div>
+                  <div className="col-12 mb-5">
+                    <div className="row">
+                      <div className="col-md-12">
+                        <button
+                          type="submit"
+                          className="btn btn-dark w-100 fw-bold"
+                        >
+                          Send
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </form>
             </div>
           </div>
-          <div className="row"></div>
         </div>
       </div>
     </>
